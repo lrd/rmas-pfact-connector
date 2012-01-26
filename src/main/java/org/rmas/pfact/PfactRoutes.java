d9037c058dba24421ca0a7c84258471dfd588ded
jw26 <james@jw26.com>
2012-01-26 22:25:41 +0000

    split + idem + enrich
    starting with the bits that look likely to be handy. testing sucks nuts
    mind you.

--- src/main/java/org/rmas/pfact/PfactRoutes.java
+++ src/main/java/org/rmas/pfact/PfactRoutes.java
@@ -1,18 +1,67 @@
 package org.rmas.pfact;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+import org.apache.camel.processor.idempotent.MemoryIdempotentRepository;
+
+import com.google.common.collect.ImmutableMap;
 
 public class PfactRoutes extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
 
-        from("timer:foo?fixedRate=true&period=10000")
-        	.setBody().constant("some stuff")
-        	.beanRef("rmasclient","pushEvent")
+        from("timer:foo?fixedRate=true&period=10000").routeId("org.rmas.pfact")
+        	.to("sql:select * from aplace").id("selectall") // the big query in here
+        	.split(body())
+        	.to("direct:splot");
+        
+        from("direct:splot") // make sure we dont see old ones
+        	.setHeader("eid").groovy("request.body.id")
+        	.idempotentConsumer(header("eid"),MemoryIdempotentRepository.memoryIdempotentRepository(200))
+        	.to("direct:idempotented");
+
+        from("direct:idempotented") // now enrich with extra bits
+        	.enrich("direct:staff", new ThisAggregationStrategy("staff"))
+        	.enrich("direct:funder", new ThisAggregationStrategy("funder"))
+        	.marshal().json(JsonLibrary.Jackson) // to json
+        	.convertBodyTo(String.class)
+        	//	.log("\n\n------ after marshal ${body.class} ${body}")
+        	.beanRef("rmasclient","pushEvent") // send
         	.to("log:org.rmas.pfact?level=DEBUG")
         	.log("pfact push ok ${body}");
+        
+        from("direct:staff")
+        	.to("sql:select staff").id("selectstaff");
+        
+        from("direct:funder")
+    		.to("sql:select funder").id("selectfunder");
 
+	}
+	
+	public class ThisAggregationStrategy implements AggregationStrategy {
+		
+		private String what;
+
+		public ThisAggregationStrategy(String what) {
+			this.what = what;
+		}
+
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		@Override
+		public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+			Map in = oldExchange.getIn().getBody(Map.class);
+			List l = newExchange.getIn().getBody(List.class);
+			Map merge = ImmutableMap.builder().putAll(in).put(what, l).build();
+			oldExchange.getOut().setBody(merge);
+			return oldExchange;
+		}
+		
 	}
 
 }
--- src/test/java/org/rmas/pfact/PfactRouteTest.java
+++ src/test/java/org/rmas/pfact/PfactRouteTest.java
@@ -1,36 +1,102 @@
 package org.rmas.pfact;
 
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import javax.naming.Context;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockComponent;
+import org.apache.camel.model.FromDefinition;
 import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.util.jndi.JndiContext;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
 public class PfactRouteTest extends CamelTestSupport {
-	
-	
+
 	@Test
 	public void convertFromSqlToJson() throws Exception {
-		context.getRouteDefinitions().get(0).adviceWith(context, new AdviceWithRouteBuilder() {
-			public void configure() throws Exception {
-				mockEndpoints("timer:foo");
-				mockEndpoints("log:org.rmas.pfact");
-			}
-		});
-    assertTrue(true);
+		final IEventEndpoint ep = mock(IEventEndpoint.class);
+		when(ep.pushEvent(anyString())).thenReturn("ok");
+
+		final ImmutableList<ImmutableMap<String, String>> allresult = ImmutableList
+				.of(ImmutableMap.of("id", "1", "title", "1 title"),
+						ImmutableMap.of("id", "1", "title", "1 title"),
+						ImmutableMap.of("id", "2", "title", "2 title"));
+
+		final ImmutableList<ImmutableMap<String, String>> staffresult = ImmutableList
+				.of(ImmutableMap.of("id", "1", "title", "some staff"),
+						ImmutableMap.of("id", "2", "title", "more staff"));
+
+		final ImmutableList<ImmutableMap<String, String>> funderresult = ImmutableList
+				.of(ImmutableMap.of("id", "1", "title", "some funder"),
+						ImmutableMap.of("id", "2", "title", "more funder"));
+		
+		context.getRouteDefinitions().get(0)
+				.adviceWith(context, new AdviceWithRouteBuilder() {
+					public void configure() throws Exception {
+						FromDefinition f = getOriginalRoute().getInputs()
+								.get(0);
+						f.setUri("direct:start");
+						weaveById("selectall").replace().setBody()
+								.constant(allresult);
+					}
+				});
+
+		context.getRouteDefinitions().get(2)
+				.adviceWith(context, new AdviceWithRouteBuilder() {
+					public void configure() throws Exception {
+						mockEndpoints("log:org.rmas.pfact.*");
+						weaveByToString(".*rmasclient.*").replace().bean(ep,
+								"pushEvent");
+					}
+				});
+
+		context.getRouteDefinitions().get(3)
+				.adviceWith(context, new AdviceWithRouteBuilder() {
+					public void configure() throws Exception {
+						weaveById("selectstaff").replace().setBody()
+								.constant(staffresult);
+					}
+				});
+
+		context.getRouteDefinitions().get(4)
+				.adviceWith(context, new AdviceWithRouteBuilder() {
+					public void configure() throws Exception {
+						weaveById("selectfunder").replace().setBody()
+								.constant(funderresult);
+					}
+				});
+
+		getMockEndpoint("mock:log:org.rmas.pfact").expectedMessageCount(2);
+
+		template.sendBody("direct:start", "something in here");
+
+		assertMockEndpointsSatisfied();
 	}
-	
+
+	@Override
+	protected Context createJndiContext() throws Exception {
+		JndiContext answer = new JndiContext();
+		IEventEndpoint ep = mock(IEventEndpoint.class);
+		answer.bind("rmasclient", ep);
+		return answer;
+	}
+
 	protected CamelContext createCamelContext() throws Exception {
 		CamelContext camelContext = super.createCamelContext();
-    org.apache.camel.impl.JndiRegistry r = (org.apache.camel.impl.JndiRegistry) ((org.apache.camel.impl.PropertyPlaceholderDelegateRegistry)camelContext.getRegistry()).getRegistry();
-    r.bind("rmasclient",new Object() {
-      public String pushEvent( String ev ) { return "nuts"; }
-    });
+		camelContext.addComponent("sql", new MockComponent());
+		camelContext.setTracing(true);
 		return camelContext;
 	}
-	
+
 	protected RouteBuilder createRouteBuilder() {
 		return new PfactRoutes();
 	}
